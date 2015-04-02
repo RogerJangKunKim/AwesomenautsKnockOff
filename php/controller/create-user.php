5e935d11b9bd55145c080de2a3e4767ef8473b30
Roger JangKun Kim <rb2stk@gmail.com>
2015-04-02 11:20:33 -0700

    Video 54. allows user to log in

---
+++ php/controller/create-user.php
@@ -0,0 +1,34 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+
+
+	$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+	// used to encrypt the password so people won't be able to identify the password.
+	$salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
+
+	// telling it to use $password and $salt use together to make an encrypted password, will give a unique encrypted password.
+
+	$hashedPassword = crypt($password, $salt);
+
+	$query = $_SESSION["connection"]->query("INSERT INTO users SET "
+		. "email = '$email',"
+		. "username = '$username',"
+		. "password = '$hashedPassword',"
+		. "salt = '$salt', "
+		. "exp = 0, "
+		. "exp1 = 0, "
+		. "exp2 = 0, "
+		. "exp3 = 0, "
+		. "exp4 = 0, ");
+
+	$_SESSION["name"] = $username;
+	if($query){
+		echo "true"; //need for Ajax on index.php
+	}
+	else{
+		echo "<p>" . $_SESSION["connection"]->error . "</p>";
+		echo "<li><a href=" . "$path" . "view/register-form.php" . ">Try Again</li>";
+	}
+
+?>
---
+++ php/controller/login-user.php
@@ -0,0 +1,38 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+
+	$array = array{
+		'exp'=> '',
+		'exp1'=> '',
+		'exp2'=> '',
+		'exp3'=> '',
+		'exp4'=> '',
+	};
+
+	$username = filter_input (INPUT_POST, "username", FILTER_SANITIZE_STRING);
+	$password = filter_input (INPUT_POST, "password", FILTER_SANITIZE_STRING);
+
+	$query = $_SESSION["connection"]->query("SELECT * FROM users WHERE username = '$username'");
+
+	//num_rows how many rows we will retrieve from database.
+	if($query->num_rows == 1){
+		$row = $query->fetch_array();
+		// case sensitive.
+		//checks if hashed password=new hashed password
+		if($row["password"] === crypt($password, $row["salt"])){
+			$_SESSION["authenticated"] = true;
+			$array["exp"] = $row["exp"]; //row is user's exp
+			$array["exp1"] = $row["exp1"];
+			$array["exp2"] = $row["exp2"];
+			$array["exp3"] = $row["exp3"];
+			$array["exp4"] = $row["exp4"];
+
+			echo json_encode($array);
+		}
+		else{
+			echo "<p>Invalid Username and Password</p>";
+		}
+	}
+	else{
+		echo "<p>Invalid Username and Password</p>";
+	}
